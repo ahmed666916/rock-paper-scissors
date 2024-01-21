5b479996f6bb81b4235c32b8c70a0ed5e0776030
ahmed666916 <ahmed666916@gmail.com>
2024-01-21 20:46:08 +0500

    created a playRound() function that plays a single round of Rock Paper Scissors

--- js/style.js
+++ js/style.js
@@ -5,7 +5,7 @@
 
 function getComputerChoice() {
 
-    let turn = ["Rock", "Paper", "Scissors"];
+    let turn = ["rock", "paper", "scissors"];
 
     let randomIndex = Math.floor(Math.random()*turn.length);
 
@@ -21,6 +21,32 @@ function playRound(playerSelection, computerSelection) {
     const resultArray = ["You Lose! Paper beats Rock", "You Lose! Scissors cuts Paper",
      "You Lose! Rock crushes Scissors", "You Win! Paper beats Rock",
      "You Win! Scissors cuts Paper","You Win! Rock crushes Scissors"];
+
+     let playerChoice = playerSelection.toLowerCase();
+     let computerChoice = computerSelection;
+     let result = "";
+
+     if (playerChoice === "rock" && computerChoice === "paper") {
+        result = resultArray[0];
+        
+     }else if (playerChoice === "paper" && computerChoice === "scissors"){
+        result = resultArray[1];
+
+     }else if(playerChoice === "scissors" && computerChoice === "rock"){
+        result = resultArray[2];
+
+     }else if(playerChoice === "paper" && computerChoice === "rock"){
+        result = resultArray[3];
+
+     }else if(playerChoice === "scissors" && computerChoice === "paper"){
+        result = resultArray[4];
+     }else if(playerChoice === "rock" && computerChoice === "scissors"){
+        result = resultArray[5];
+     }else{
+        result = "Tied! Try Again."
+     }
+
+     return result;
 
 }
 
