806fdc9af4d1d41f0edeb4d7688bbd43a0ef3d0d
ahmed666916 <ahmed666916@gmail.com>
2024-01-21 19:24:07 +0500

    created getComputerChoice() function

--- js/style.js
+++ js/style.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+
+
+
+
+
+function getComputerChoice() {
+
+    let turn = ["rock", "paper", "scissor"];
+
+    let randomIndex = Math.floor(Math.random()*turn.length);
+
+    let randomChoice = turn[randomIndex];
+
+    return randomChoice;
+
+}
+
+console.log(getComputerChoice());
+
+
